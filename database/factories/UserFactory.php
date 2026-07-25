e6e7d91d27e4932057b0f5601a684df056ffe176
Farhan Palilati <146071772+FarhanPalilat@users.noreply.github.com>
2026-07-25 23:24:57 +0800

    testing faker v3

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,11 +23,9 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        $faker = \Faker\Factory::create();
-
         return [
-            'name' => $faker->name(),
-            'email' => $faker->unique()->safeEmail(),
+            'name' => 'Administrator E-Pokir',
+            'email' => 'admin@gmail.com',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
